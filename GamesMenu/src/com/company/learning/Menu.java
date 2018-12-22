c6d0955dc9196bf1f5833b724072697b3fc8a877
Szabo Lorand <44921643+NoName2002@users.noreply.github.com>
2018-12-22 19:49:39 +0200

    Add files via upload

--- GamesMenu/src/com/company/learning/Menu.java
+++ GamesMenu/src/com/company/learning/Menu.java
@@ -6,10 +6,12 @@ public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n;
-        System.out.print("Select a program to start: \n 0 - Exit Menu \n 1 - Numberguesser Game \n 2 - Tic Tac Toe \n");
-        n = scanner.nextInt();
-        while(n != 0){
-            if(n > 0 && n < 3) {
+        do {
+            System.out.println("\f");
+            System.out.print("Select a program to start: \n 0 - Exit Menu \n 1 - Numberguesser Game \n 2 - Tic Tac Toe \n");
+            System.out.println(" 3 - Hangman");
+            n = scanner.nextInt();
+            if (n > 0 && n < 4) {
                 switch (n) {
                     case 1:
                         System.out.println("Starting Numberguesser...");
@@ -18,13 +20,15 @@ public class Menu {
                     case 2:
                         System.out.println("Starting Tic Tac Toe...");
                         (new Tictactoe()).startTictactoe();
+                        break;
+                    case 3:
+                        System.out.println("Starting Hangman...");
+                        (new Hangman()).startHangman();
                 }
-            }
-            else{
+            } else {
                 System.out.println("Wrong input! Please enter a different number!");
                 n = scanner.nextInt();
             }
-        }
+        } while (n != 0);
     }
-
 }
